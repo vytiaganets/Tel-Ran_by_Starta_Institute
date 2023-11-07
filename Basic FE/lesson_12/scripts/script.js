62516f82fb8147f8fd506e9c5996259ed78da07b
Andrii <vytiaganets@ukr.net>
2023-11-07 12:48:25 +0100

    FE lesson_12

---
+++ Basic FE/lesson_12/scripts/script.js
@@ -0,0 +1,40 @@
+// let greeting = "Hello world"
+// const greeting = "Hello world"
+// console.log(greeting)
+
+// --------------name-------------
+// A-Z a-z 0-9 _ $
+// let 1person = "John" // error
+// let _name = "Name"
+// let $name = "John"
+// carr model name
+// contacts-block // kebab case // error
+// let car_model_name = "BMW" // snake case
+//let carModelName = "BMW" // camel case
+
+// --------------number, string-------------
+// typeof показывает тип данных значения переменной
+// let text = "Hello"
+// let num1 = 5
+// let num2 = -5
+// let num3 = 0.56
+// console.log(text, num1, num2, num3)
+// console.log(typeof text)
+// console.log(typeof num1)
+// console.log(typeof num2)
+// console.log(typeof num3)
+
+// -------------math operator------------
+// let a = 5;
+// let b = 3;
+// console.log(a + b) // сложение
+// console.log(a - b) // вычитание
+// console.log(a * b) // умножение
+// console.log(a / b) // деление
+// console.log(a % b) // получить остаток от деления
+// console.log(a ** b) // возведение в степень
+
+// -------------prompt------------
+// prompt сщздает модальное окно, чтобы забрать какие-то значения от пользователя
+let userName = prompt("Напишите свое имя.")
+console.log("Hello " + userName);
