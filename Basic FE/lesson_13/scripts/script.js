72119766c401ab699b0288482ec66e842fb076e4
Andrii <vytiaganets@ukr.net>
2023-11-08 14:18:58 +0100

    FE lesson 12&13

---
+++ Basic FE/lesson_13/scripts/script.js
@@ -0,0 +1,18 @@
+//--------------------arrays--------------------
+let arr = [4, 6, 12, 0, 36, "Hello", true, 7]
+// console.log(arr)
+// console.log(arr[5])//Hello
+// console.log(arr[2])//12
+// console.log(arr[8])//undefined
+// console.log(arr.length)//длину массива (количество элементов)
+// console.log(arr[arr.length-1]) //7
+// arr[5] = "World"
+// console.log(arr[5]) //Hello
+// arr[2] = arr
+// console.log(arr)
+// let arr2 =[[1,4,4,6,5],[1,6,5,3]]
+// console.log(arr2[1][3])//3
+
+// push pop
+arr.push(15, "John", -9, 65) // добавить элементы в массив в конец
+console.log(arr)
