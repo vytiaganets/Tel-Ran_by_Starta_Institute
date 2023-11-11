046cd54c40020da2cb81a17568200c84cda656ad
Andrii <vytiaganets@ukr.net>
2023-11-11 21:35:54 +0100

    base JS

--- Basic FE/javasript-beginner/app.js
+++ Basic FE/javasript-beginner/app.js
@@ -1,11 +1,27 @@
-// 1 переменные
+// Number
+// const num = 42 // integer
+// const float = 42.42 // float
+// const pow = 10e3
+// console.log(pow) // 10000
+// Number.MAX_SAAFE_INTEGER
+// console.log(Number.MAX_SAFE_INTEGER) // 9007199254740991
+// console.log(Math.pow(2, 53)-1) // 9007199254740991
+// console.log(Number.MIN_SAFE_INTEGER) // -9007199254740991
+// const negative = -42
 
-var sname = 'Andrii'
-const lastName = 'Vyt'
-let age = 26
 
-//name = 'Andr'
+// console.log('Number.MAX_VALUE: ', Number.MAX_VALUE) // 1.7976931348623157e+308
+// console.log('Number.MIN_VALUE: ', Number.MIN_VALUE) // 5e-324
+// console.log('POSITIVE_INFINITY: ', Number.POSITIVE_INFINITY) // // Infinity
+// console.log('NEGATIVE_INFINITY: ', Number.NEGATIVE_INFINITY) // -Infinity
+// console.log('2/0: ', 2 / 0) // Infinity
+// console.log(Number.NaN)
+// console.log(typeof NaN) // number
+// console.log(2 / undefined) // NaN
+// const weid = 2 / undefined
+// console.log(Number.isNaN(weid)) // true
+// console.log(Number.isNaN(42)) // false
+// console.log(Number.isFinite(Infinity)) // false
+// console.log(Number.isFinite(42)) // true
 
-console.log(age)
-
-//00*34*30
+// 1:58:24
