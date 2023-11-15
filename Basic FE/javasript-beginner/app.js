b809fcdff083db7ec6a800c57516e9767caf7255
Andrii <vytiaganets@ukr.net>
2023-11-15 17:09:07 +0100

    JS base

--- Basic FE/javasript-beginner/app.js
+++ Basic FE/javasript-beginner/app.js
@@ -3,13 +3,13 @@
 //   age: 26,
 //   isProgrammer: true,
 //   languages: ['ru', 'en', 'de'],
-//   'complex key': 'Complex value',
-//   ['key_'+(1+3)]: 'Computed Key', //key_4
-//   // greet: function(){
-//   // }
-//   greet() {
-//     console.log('greet from person')
-//   }
+  // 'complex key': 'Complex value',
+  // ['key_'+(1+3)]: 'Computed Key', //key_4
+  // greet: function(){
+  // }
+  // greet() {
+  //   console.log('greet from person')
+  // }
 // }
 
 // console.log(person.name); //Andrii
@@ -39,5 +39,44 @@
 // const {name, age: personAge = 10, languages} = person
 // console.log(name, personAge, languages);
 
+//console.log(person);
+// for (let key in person) {
+//   if (person.hasOwnProperty(key)) {
+//     console.log('key: ', key);
+//     console.log('value: ', person[key]);
+//   }
+// }
 
-03:56:45
+// const keys = Object.keys(person)
+// 
+
+// keys.forEach((key) => {
+//   console.log(key);
+//   console.log('value: ', person[key]);
+// })
+
+// Context
+
+const person = {
+  name: 'Andrii',
+  age: 26,
+  isProgrammer: true,
+  languages: ['ru', 'en', 'de'],
+  info(){
+    console.log('this: ', this);
+    console.info('Информация про человека по имени: ', this.name)
+  }
+}
+//person.info()
+
+const logger = {
+  keys(){
+    console.log('Object Keys: ', Object.keys(this));
+  }
+}
+// logger.keys(person)
+//const bound = logger.keys.bind(logger)
+//bound() // Object Keys: ["keys"]
+//console.log(bound);
+const bound = logger.keys.bind(person)
+bound() //Object Keys: ["name", "age", "isProgrammer",...]
