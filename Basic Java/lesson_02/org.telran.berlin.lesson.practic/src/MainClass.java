6825208c3a49814028aca16967d5c8b2fe7fcb3f
Andrii <vytiaganets@ukr.net>
2023-11-28 13:04:33 +0100

    refactoring 28_11_23

---
+++ Basic Java/lesson_02/org.telran.berlin.lesson.practic/src/MainClass.java
@@ -0,0 +1,77 @@
+class MainClass {
+    public static void main(String[] args) {
+//     psvm
+//     source root выбрать обязательно кликнув по src
+//     System.out.println("Hello world!");
+        // Boolean
+//        boolean isBlack = true; // false
+        /*Numbers*/
+//        byte b = 13; // -128 ... 127
+//        short s = 1000; // -32 768... 32 767
+//        int i = 6477476;
+//        long l = 68_698_696;
+        // Float, Double
+//        float f = 1.234f;
+//        double d = 1.299797;
+        // Char
+//        char ch1 = 'q';
+//        char ch2 = '\u0000';
+        //System.out.println(ch2);
+
+        //String
+//        String str = "Hello \"world\"";
+//        System.out.println(str);
+
+
+        char data[] = {'a', 'b', 'c'};
+        String str1 = new String(data);
+        String str11 = new String("bca");
+        String str2 = "abc";
+        System.out.println(str2);
+
+
+//        int x1 = 5;
+//        float x2 = 2;
+//        float res = x1 / x2;
+//        int res2 = x1 % 2;
+//        System.out.println(res);
+//        System.out.println(res2);
+
+//        int x3 = 7;
+//        int x4 = 4;
+//        int multRes = x3 * x4;
+//        int addRes = x3 + x4;
+//        int pow3Res = x3 *x3 * x3;
+//        System.out.println(multRes);
+//        System.out.println(addRes);
+//        System.out.println(pow3Res);
+
+//
+//        int  a =7;
+//        float c = 7.0f;
+//        System.out.println(a>c); //false
+
+        // byte -> short -> int -> long
+        // float -> double
+//        long l1 = 1234;
+//        int i2 = (int)l1;
+//        int i3 = 12;
+//        long l3 = i3;
+//        System.out.println(l3); //12
+
+//        int i2 = 1000;
+//        byte b2 = (byte) i2;
+//        System.out.println(i2); // 1000
+//        System.out.println(b2); //-24
+
+//        int i2 = 127;
+//        byte b2 = (byte) i2;
+//        System.out.println(i2); // 127
+//        System.out.println(b2); // 127
+
+          //Integer i3 = new Integer(123);
+         //System.out.println(i2>i3);
+          //List<Integer>
+
+    }
+}
