2af18a7d9f2e0c371853c3b961e8f17d22cbd356
Andrii <vytiaganets@ukr.net>
2023-11-30 14:00:02 +0100

    Base BE lesson_04

---
+++ Basic Java/lesson_04/src/Types.java
@@ -0,0 +1,13 @@
+public class Types {
+    public static void main(String[] args) {
+       // преобразование типов
+//       byte num = 113;
+//        System.out.println(num); //113
+//        byte num1 = (byte)130; // -128 -127 -126 ... 127 если 128 (-128). 129(-127) . если 130 (-126) ... //явное преобразование
+//        System.out.println(num1); //-126
+//        short num2 = (short) 55000;
+//        System.out.println(num2);//-10536
+        System.out.println("awas");
+        System.out.print("nlnl ");
+    }
+}
