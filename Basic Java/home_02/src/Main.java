7126ba9cbf586a74fa53271b46a845920ff6ee10
Andrii <vytiaganets@ukr.net>
2023-11-30 17:43:46 +0100

    BE home_02

---
+++ Basic Java/home_02/src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Описание задания.
+//        1 уровень сложности: Домашнее задание 2.
+//==========================№ 1
+//
+//
+//        В методе main инициализировать все примитивные типы и не примитивные типы.
+//
+//                Вывести их результат в консоль:
+//        char: G
+//        int: 89
+//        byte: 4
+//        short: 56
+//        float: 4.7333436
+//        double: 4.355453532
+//        long: 12121
+//    char g = 'G';
+//    int num1 = 89;
+//    byte num2 = 4;
+//    short num3 = 56;
+//    float num4 = 4.7333436f;
+//    double num5 = 4.355453532;
+//    long num6 = 12121;
+//        System.out.println("Character: " + g);
+//        System.out.println("Integer: " + num1);
+//        System.out.println("Byte: " + num2);
+//        System.out.println("Short: " + num3);
+//        System.out.println("Float: " + num4);
+//        System.out.println("Double: " + num5);
+//        System.out.println("Long: " + num6);
+//
+//        Character: G
+//        Integer: 89
+//        Byte: 4
+//        Short: 56
+//        Float: 4.7333436
+//        Double: 4.355453532
+//        Long: 12121
+
+//============================№ 2*
+//                Дано трехзначное число. Вывести на экран все цифры этого числа.
+//
+//                Пример: 345
+//        Вывод в консоль: Число 345 -> 3, 4, 5
+//        int n = 345;
+//        System.out.println("Вывод в консоль: Число 345 -> " + n%1000/100 + ", " + n%100/10 + ", " + n%10);//Вывод в консоль: Число 345 -> 3, 4, 5
+//        2 уровень сложности:
+
+// ============================№ 3*
+//        В методе main добавить инструкцию System.out.println(); необходимое количество раз, чтобы в консоль распечатался текст:
+//        Hello,
+//                my
+//        name
+//                is
+//        <your name>!
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите имя: ");//The weather is good today
+//        String str1 = sc.nextLine();
+//        System.out.println("Hello,");
+//        System.out.println("        my");
+//        System.out.println("name");
+//        System.out.println("        is");
+//        System.out.println(str1 + "!");
+//                Используя инструкцию System.out.print(); распечатайте в консоль текст: Hello, my name is <your name>!
+//        System.out.print("Hello, my name is " + str1 + "!");
+// ============================№ 3*
+//        1. Создать пакет game
+//        2. В пакете дате создать пакет hero
+//        3. Создать класс Elf
+//        4. Создать поле int id
+//        5. Создать еще 5 полей (любого типа)
+//        6. Создать методы get<имяПоля> и set<имяПоля> для всех полей, подумать и добавить логику в эти методы.
+//        Например для поля id: getId(); setld (int newId);
+
+    }
+}
---
+++ Basic Java/home_02/src/game/hero/Elf.java
@@ -0,0 +1,46 @@
+package game.hero;
+
+public class Elf {
+    int newId;
+    char newG = 'G';
+    int newNum1 = 89;
+    byte newNum2 = 4;
+    short newNum3 = 56;
+    float newNum4 = 4.7333436f;
+    public void getId(){
+
+    }
+    public void setId (int newId){
+        this.newId=newId;
+    }
+    public void getNewG(){
+
+    }
+    public void setNewG (char newG){
+        this.newG=newG;
+    }
+    public void getNewNum1(){
+
+    }
+    public void setNewNum1 (char newNum1){
+        this.newNum1=newNum1;
+    }
+    public void getNewNum2(){
+
+    }
+    public void setNewNum2 (byte newNum2){
+        this.newNum2=newNum2;
+    }
+    public void getNewNum3(){
+
+    }
+    public void setNewNum3 (short newNum3){
+        this.newNum3=newNum3;
+    }
+    public void getNewNum4(){
+
+    }
+    public void setNewNum4 (float newNum4){
+        this.newNum4=newNum4;
+    }
+}
