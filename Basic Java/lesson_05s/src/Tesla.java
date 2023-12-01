c82015c9ee7ff2dbd7c75d82224bff51246ae357
Andrii <vytiaganets@ukr.net>
2023-12-01 12:54:15 +0100

    01_12_23 refactoring

---
+++ Basic Java/lesson_05s/src/Tesla.java
@@ -0,0 +1,9 @@
+public class Tesla {
+    public static int numberWeels = 4;
+    public static boolean isElectric = true;
+    public static int maxSpeed;
+    public int chargeValue;
+    public void charge(int time, int power){
+        //
+    }
+}
