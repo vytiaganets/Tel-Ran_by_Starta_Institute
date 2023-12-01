298831db7453fc3a8ebc98d0764b2c14c15aa6ba
Andrii <vytiaganets@ukr.net>
2023-12-01 12:02:32 +0100

    Base Java refactoring 01_12_23

---
+++ Basic Java/lesson_05s/src/Main1.java
@@ -0,0 +1,11 @@
+public class Main1 {
+    private void test(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Main1 main = new Main1();
+        main.test();
+    }
+}
