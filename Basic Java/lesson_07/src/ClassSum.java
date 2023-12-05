b0ee8989e561e2858bb84c295f6302a81e74a23c
Andrii <vytiaganets@ukr.net>
2023-12-05 13:10:13 +0100

    BE Base refactoring 05_12_23

---
+++ Basic Java/lesson_07/src/ClassSum.java
@@ -0,0 +1,8 @@
+public class ClassSum {
+    public int sum(int x, int y){
+        return x + y;
+    }
+    public double sum(int x, double y){
+        return x + y;
+    }
+}
---
+++ Basic Java/lesson_07/src/Program.java
@@ -0,0 +1,10 @@
+public class Program{
+    public static void main(String[] args) {
+        ClassSum sumNum = new ClassSum();
+        int res = sumNum.sum(2, 3);
+        double res1 = sumNum.sum(4, 3);
+        System.out.println(res);
+        System.out.println(res1);
+    }
+
+}
