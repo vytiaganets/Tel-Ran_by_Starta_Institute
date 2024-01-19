a20c8008d1e881013648d5d95d5f7573bc1ed025
Andrii <vytiaganets@ukr.net>
2024-01-19 12:57:00 +0100

    JavaPro 18_01_2024

---
+++ JavaPro171023/src/main/java/de/telran/lesson20240119/Car.java
@@ -0,0 +1,12 @@
+package de.telran.lesson20240119;
+
+public class Car extends Vehicle {
+int doors;
+String bodyType;
+
+    public Car(String vin, String model, String color, int power, int year, int doors, String bodyType) {
+        super(vin, model, color, power, year);
+        this.doors = doors;
+        this.bodyType = bodyType;
+    }
+}
---
+++ JavaPro171023/src/main/java/de/telran/lesson20240119/Vehicle.java
@@ -0,0 +1,17 @@
+package de.telran.lesson20240119;
+
+public class Vehicle {
+    String vin;
+    String model;
+    String color;
+    int power;
+    int year;
+
+    public Vehicle(String vin, String model, String color, int power, int year) {
+        this.vin = vin;
+        this.model = model;
+        this.color = color;
+        this.power = power;
+        this.year = year;
+    }
+}
