27f0922a254b99bc032b1d6500f4752a05ed88a8
Andrii <vytiaganets@ukr.net>
2024-04-08 19:33:49 +0200

    08.04.2024 refactoring

---
+++ JavaPro171023/src/main/java/de/telran/home20240321/Client.java
@@ -0,0 +1,101 @@
+package de.telran.home20240321;
+
+public class Client {
+    private Long id;
+    private String status;
+    private Long taxCode;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phone;
+
+    public Client(Long id, String status, Long taxCode, String firstName, String lastName, String email, String address, String phone) {
+        this.id = id;
+        this.status = status;
+        this.taxCode = taxCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTaxCode() {
+        return taxCode;
+    }
+
+    public void setCode(Long taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", taxCode=" + taxCode +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
