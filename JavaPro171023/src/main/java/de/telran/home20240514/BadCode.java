7b6ddbf84c48eb1a33eab1ce3d478def6d3e6584
Andrii <vytiaganets@ukr.net>
2024-05-21 20:11:20 +0200

    21.05.2024 refactoring

---
+++ JavaPro171023/src/main/java/de/telran/home20240514/BadCode.java
@@ -0,0 +1,14 @@
+package de.telran.home20240514;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+public @interface BadCode {
+
+    String name();
+    String comment();
+}
