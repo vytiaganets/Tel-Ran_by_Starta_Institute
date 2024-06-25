bd2ec3fca05d4c57db1f7ca2dc9fac5586836106
Andrii <vytiaganets@ukr.net>
2024-06-25 13:56:54 +0200

    25.06.2024 refactoring

--- telran-bank-171023/src/main/resources/config/liquibase/sql/001_create_account.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
---changeset marat:1
-
-create table account (
-    id bigint not null primary key
-);
--- telran-bank-171023/src/main/resources/config/liquibase/sql/002_add_auto_increment.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset marat:2
-
-alter table account
-    alter column id bigint auto_increment;
