2f72b2ba5931b3f460e72515c0f134bc5c9634d8
Andrii <vytiaganets@ukr.net>
2024-06-25 13:28:46 +0200

    spring1: modify project

--- onlineshop/src/main/java/com/example/onlineshop/repository/CategoryJpaRepository.java
+++ onlineshop/src/main/java/com/example/onlineshop/repository/CategoryJpaRepository.java
@@ -3,6 +3,9 @@ package com.example.onlineshop.repository;
 import com.example.onlineshop.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
-CategoryEntity findByName(String name);
+    //CategoryEntity findByName(String name);
+    Optional<CategoryEntity> findByName(String name);
 }
